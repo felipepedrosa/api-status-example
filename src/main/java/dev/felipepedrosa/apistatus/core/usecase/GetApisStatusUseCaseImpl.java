3e3ed32fd08b49f598883793eeebe0dfa38d0b66
felipepedrosa <felipeepedrosa@gmail.com>
2024-06-03 13:46:52 -0300

    Add Scheduled execution

--- src/main/java/dev/felipepedrosa/apistatus/core/usecase/GetApisStatusUseCaseImpl.java
+++ src/main/java/dev/felipepedrosa/apistatus/core/usecase/GetApisStatusUseCaseImpl.java
@@ -3,17 +3,29 @@ package dev.felipepedrosa.apistatus.core.usecase;
 import dev.felipepedrosa.apistatus.core.domain.ApiStatus;
 import dev.felipepedrosa.apistatus.core.domain.EnumApis;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class GetApisStatusUseCaseImpl implements UseCase<Map<EnumApis, ApiStatus>> {
-    public final ApisStatusContext apisStatusContext;
+    private final ApisStatusContext apisStatusContext;
+
+    private Map<EnumApis, ApiStatus> apisStatus = Collections.emptyMap();
 
     @Override
     public Map<EnumApis, ApiStatus> execute() {
-        return apisStatusContext.getApisStatus();
+        return apisStatus;
+    }
+
+    @Scheduled(fixedRate = 300000)
+    private void getApiStatus() {
+        log.info("Getting apis status");
+        apisStatus = apisStatusContext.getApisStatus();
     }
 }
